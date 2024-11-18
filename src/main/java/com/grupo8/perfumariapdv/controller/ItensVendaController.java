f647b8d2e2841ae2e14577077e2ab9b78c4914e0
Arthur Vital <118780101+arthurvtl@users.noreply.github.com>
2024-11-18 15:02:32 -0300

    Add files via upload

---
+++ src/main/java/com/grupo8/perfumariapdv/controller/ItensVendaController.java
@@ -0,0 +1,42 @@
+package com.grupo8.perfumariapdv.controller;
+
+import com.grupo8.perfumariapdv.db.ItensVendaDao;
+import com.grupo8.perfumariapdv.mock.ItensVendaMock;
+import com.grupo8.perfumariapdv.model.ItemVenda;
+import java.util.List;
+
+
+public class ItensVendaController {
+    
+    //SALVAR
+    public static String salvar(ItemVenda itensVenda){
+        
+        String resposta = null;
+        
+        //Tenta fazer a insercao
+        try{
+            //ItensVendaMock.salvar(itensVenda);
+            ItensVendaDao.salvar(itensVenda);
+        }catch (Exception e){
+            e.printStackTrace();
+            resposta = "Erro na fonte de dados";
+        }
+        
+        return resposta;
+    }
+    
+    //OBTER ITENS DA VEMDA
+    public static List<ItemVenda> obterItens(int idVenda){
+        List<ItemVenda> listaResposta = null;
+        
+        //Tenta fazer a busca dos dados 
+        try{
+            //listaResposta = ItensVendaMock.obterItens(idVenda);
+            listaResposta = ItensVendaDao.obterItens(idVenda);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return listaResposta;
+    }
+}
